449aec67877dd3a25aad289797ad67276cf6586b
suslmk <67575226+suslmk-lee@users.noreply.github.com>
2022-08-18 06:48:55 +0900

    Set up the Touchdown iOS project in Xcode

---
+++ Touchdown/Utility/Constant.swift
@@ -0,0 +1,23 @@
+//
+//  Constant.swift
+//  Touchdown
+//
+//  Created by minkyuLee on 2022/08/18.
+//
+
+import SwiftUI
+
+// DATA
+
+// COLOR
+
+let colorBackground: Color = Color("ColorBackground")
+let colorGray: Color = Color(UIColor.systemGray4)
+
+// LAYOUT
+// UX
+// API
+// IMAGE
+// FONT
+// STRING
+// MISC
